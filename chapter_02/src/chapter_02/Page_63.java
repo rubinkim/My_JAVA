3947876bb68cf528cd8eabc513a3b60bb312516b
rubinkim <kimyongha2011@gmail.com>
2024-05-19 16:49:04 +0900

    chapter_02_데이터활용

---
+++ chapter_02/src/chapter_02/Page_63.java
@@ -0,0 +1,12 @@
+package chapter_02;
+
+public class Page_63 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int depositAmount;
+		depositAmount = 50000000;
+		System.out.println(depositAmount);		
+	}
+
+}
