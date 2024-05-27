29d01c9072cb3d966cae341fe73ccc3fbd9b16cc
rubinkim <kimyongha2011@gmail.com>
2024-05-27 16:22:13 +0900

    practice a variety of operators

---
+++ chapter_02/src/chapter_02/Page_75.java
@@ -0,0 +1,16 @@
+package chapter_02;
+
+public class Page_75 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 증감연산자
+		int a = 10;
+		int b = a++;
+		int c = ++a;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
