8b65fbb4136eacaa3884af134a5f2746e207a251
rubinkim <kimyongha2011@gmail.com>
2024-05-27 16:59:56 +0900

    practice a variety of operators

--- chapter_02/src/chapter_02/Page_86.java
+++ chapter_02/src/chapter_02/Page_86.java
@@ -9,8 +9,10 @@ public class Page_86 {
 		int b = 0b11111111111111111111111111110011;
 		int c = 0b00000000000000000000000000000001;
 		int d = b + c;
+		System.out.println(d);
 		String e = Integer.toBinaryString(d);
 		System.out.println(Integer.toBinaryString(b+c));
+		System.out.println(d);
 		System.out.println(e);
 	}
 
