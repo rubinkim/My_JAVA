79d00952cb54dd0271e20dc0d8f6a336d3dabb48
rubinkim <kimyongha2011@gmail.com>
2024-05-27 16:57:01 +0900

    practice a variety of operators

---
+++ chapter_02/src/chapter_02/Page_86.java
@@ -0,0 +1,17 @@
+package chapter_02;
+
+public class Page_86 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 비트연산자
+		int a = 0b00000000000000000000000000001100;
+		int b = 0b11111111111111111111111111110011;
+		int c = 0b00000000000000000000000000000001;
+		int d = b + c;
+		String e = Integer.toBinaryString(d);
+		System.out.println(Integer.toBinaryString(b+c));
+		System.out.println(e);
+	}
+
+}
