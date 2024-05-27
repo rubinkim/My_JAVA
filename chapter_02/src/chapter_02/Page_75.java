ce0787437baf5711f8b2795254330b3fc4cf02d2
rubinkim <kimyongha2011@gmail.com>
2024-05-27 16:25:57 +0900

    practice a variety of operators

--- chapter_02/src/chapter_02/Page_75.java
+++ chapter_02/src/chapter_02/Page_75.java
@@ -11,6 +11,11 @@ public class Page_75 {
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(c);
+		
+		// 부호연산자 사용시 주의할 점은 부호정보변경은 기본적으로 int타입으로 처리된다는 것이다.
+		short d = 10;
+		short e = (short)-d;  // Type mismatch: cannot convert from int to short
+		System.out.println(e);
 	}
 
 }
